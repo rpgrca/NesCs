9575a49d6acd373955faf5cb78922cf0cd5a4785
Roberto <rpgrca@gmail.com>
2023-07-20 23:59:13 -0300

    ad test, load bus conflicts

--- NesCs.Logic/Flags10.cs
+++ NesCs.Logic/Flags10.cs
@@ -4,13 +4,16 @@ public readonly struct Flags10
 {
     private const int TvSystemFlag = 0b11;
     private const int ProgramRamPresentFlag = 0b10000;
+    private const int BoardHasBusConflictsFlag = 0b100000;
 
     public int TvSystem { get; }
     public bool HasProgramRam { get; }
+    public bool HasBusConflicts { get; }
 
     public Flags10(int flags)
     {
         TvSystem = flags & TvSystemFlag;
         HasProgramRam = (flags & ProgramRamPresentFlag) == ProgramRamPresentFlag;
+        HasBusConflicts = (flags & BoardHasBusConflictsFlag) == BoardHasBusConflictsFlag;
     }
 }
