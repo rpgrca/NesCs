b385ba0e3254c2fc8f8acc4c95662e9e9568e60d
Roberto <rpgrca@gmail.com>
2023-07-20 22:46:28 -0300

    add test, load trainer flag correctly

--- NesCs.Logic/Flags6.cs
+++ NesCs.Logic/Flags6.cs
@@ -4,13 +4,16 @@ public struct Flags6
 {
     private const int MirroringFlag = 0x1;
     private const int BatteryBackedProgramRamFlag = 0x2;
+    private const int TrainerFlag = 0x4;
 
     public Mirroring Mirroring { get; }
     public bool HasBatteryBackedProgramRam { get; }
+    public bool HasTrainer { get; }
 
     public Flags6(int flags)
     {
         Mirroring = (Mirroring)(flags & MirroringFlag);
         HasBatteryBackedProgramRam = (flags & BatteryBackedProgramRamFlag) == BatteryBackedProgramRamFlag;
+        HasTrainer = (flags & TrainerFlag) == TrainerFlag;
     }
 }
--- NesCs.UnitTests/NesFileMust.cs
+++ NesCs.UnitTests/NesFileMust.cs
@@ -25,19 +25,28 @@ public class NesFileMust
         Assert.Contains("Signature not found", exception.Message);
     }
 
-    [Fact]
-    public void CreateNesFile_WhenFileHasValidHeader()
+    [Theory]
+    [MemberData(nameof(NesFileHeaderFeeder))]
+    public void CreateNesFile_WhenFileHasValidHeader(byte[] fileContents, int expectedProgramRomSize, int expectedCharacterRomSize, Mirroring expectedMirroring,
+        bool expectedBatteryBackup, bool expectedTrainer)
     {
-        var fileStub = new MockFileData(Constants.GetValidNesHeaderFile());
+        var fileStub = new MockFileData(fileContents);
         var proxy = FileSystemProxy.CreateWith(new MockFileSystem(new Dictionary<string, MockFileData>() { { NES_FILENAME, fileStub } }));
         var sut = proxy.Load(NES_FILENAME);
 
         Assert.NotNull(sut);
         Assert.Equal(NES_FILENAME, sut.Filename);
-        Assert.Equal(32, sut.ProgramRomSize);
-        Assert.Equal(16, sut.CharacterRomSize);
-        Assert.Equal(Mirroring.Vertical, sut.Flags6.Mirroring);
-        Assert.True(sut.Flags6.HasBatteryBackedProgramRam);
+        Assert.Equal(expectedProgramRomSize, sut.ProgramRomSize);
+        Assert.Equal(expectedCharacterRomSize, sut.CharacterRomSize);
+        Assert.Equal(expectedMirroring, sut.Flags6.Mirroring);
+        Assert.Equal(expectedBatteryBackup, sut.Flags6.HasBatteryBackedProgramRam);
+        Assert.Equal(expectedTrainer, sut.Flags6.HasTrainer);
+    }
+
+    public static IEnumerable<object[]> NesFileHeaderFeeder()
+    {
+        yield return new object[] { new byte[] { 0x4e, 0x45, 0x53, 0x1A, 0x20, 0x10, 0b1000011, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, 32, 16, Mirroring.Vertical, true, false };
+        yield return new object[] { new byte[] { 0x4e, 0x45, 0x53, 0x1A, 0x20, 0x10, 0b0111100, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, 32, 16, Mirroring.Horizontal, false, true };
     }
 }
 
@@ -45,7 +54,4 @@ public static class Constants
 {
     public static byte[] GetEmptyNesHeaderFile() =>
         new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-
-    public static byte[] GetValidNesHeaderFile() =>
-        new byte[] { 0x4e, 0x45, 0x53, 0x1A, 0x20, 0x10, 0b1000011, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 }
