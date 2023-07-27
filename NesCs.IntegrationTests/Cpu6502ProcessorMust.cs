9118477d2b50a092094e1c33161754e106c267e1
Roberto <rpgrca@gmail.com>
2023-07-27 15:39:31 -0300

    add integration tests for a9

--- NesCs.IntegrationTests/Cpu6502ProcessorMust.cs
+++ NesCs.IntegrationTests/Cpu6502ProcessorMust.cs
@@ -6,43 +6,11 @@ public class Cpu6502ProcessorMust
 {
 	[Theory]
     [ClassData(typeof(OpcodeFeeder<OpcodeA5>))]
-    public void Execute10000DifferentA5SampleTestsCorrectly(SampleCpuTest data)
-    {
-        var trace = new List<(int, byte, string)>();
-		var sut = Utilities.CreateSubjectUnderTestFromSample(data, trace);
-        sut.Run();
-
-        Utilities.Equal(data.Final, sut);
-        Utilities.Equal(data.Cycles, trace);
-    }
-
-	[Theory]
+    [ClassData(typeof(OpcodeFeeder<OpcodeA9>))]
     [ClassData(typeof(OpcodeFeeder<OpcodeAD>))]
-    public void Execute10000DifferentADSampleTestsCorrectly(SampleCpuTest data)
-    {
-        var trace = new List<(int, byte, string)>();
-		var sut = Utilities.CreateSubjectUnderTestFromSample(data, trace);
-        sut.Run();
-
-        Utilities.Equal(data.Final, sut);
-        Utilities.Equal(data.Cycles, trace);
-    }
-
-	[Theory]
     [ClassData(typeof(OpcodeFeeder<OpcodeB1>))]
-    public void Execute10000DifferentB1SampleTestsCorrectly(SampleCpuTest data)
-    {
-        var trace = new List<(int, byte, string)>();
-		var sut = Utilities.CreateSubjectUnderTestFromSample(data, trace);
-        sut.Run();
-
-        Utilities.Equal(data.Final, sut);
-        Utilities.Equal(data.Cycles, trace);
-    }
-
-	[Theory]
     [ClassData(typeof(OpcodeFeeder<OpcodeB5>))]
-    public void Execute10000DifferentB5SampleTestsCorrectly(SampleCpuTest data)
+    public void Execute10000DifferentSampleTestsCorrectly(SampleCpuTest data)
     {
         var trace = new List<(int, byte, string)>();
 		var sut = Utilities.CreateSubjectUnderTestFromSample(data, trace);
@@ -52,4 +20,3 @@ public class Cpu6502ProcessorMust
         Utilities.Equal(data.Cycles, trace);
     }
 }
-
