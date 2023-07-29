cdb45aaf367365572b87da758ac5c590818ed17a
Roberto <rpgrca@gmail.com>
2023-07-29 13:20:20 -0300

    simplify integration test feeder, hardcode path in feeder

--- NesCs.IntegrationTests/Cpu6502ProcessorMust.cs
+++ NesCs.IntegrationTests/Cpu6502ProcessorMust.cs
@@ -5,24 +5,24 @@ namespace NesCs.IntegrationTests;
 public class Cpu6502ProcessorMust
 {
 	[Theory]
-    [ProcessorFileTestData(typeof(OpcodeA0))]
-    [ProcessorFileTestData(typeof(OpcodeA1))]
-    [ProcessorFileTestData(typeof(OpcodeA2))]
-    [ProcessorFileTestData(typeof(OpcodeA4))]
-    [ProcessorFileTestData(typeof(OpcodeA5))]
-    [ProcessorFileTestData(typeof(OpcodeA6))]
-    [ProcessorFileTestData(typeof(OpcodeA9))]
-    [ProcessorFileTestData(typeof(OpcodeAC))]
-    [ProcessorFileTestData(typeof(OpcodeAD))]
-    [ProcessorFileTestData(typeof(OpcodeAE))]
-    [ProcessorFileTestData(typeof(OpcodeB1))]
-    [ProcessorFileTestData(typeof(OpcodeB4))]
-    [ProcessorFileTestData(typeof(OpcodeB5))]
-    [ProcessorFileTestData(typeof(OpcodeB6))]
-    [ProcessorFileTestData(typeof(OpcodeB9))]
-    [ProcessorFileTestData(typeof(OpcodeBC))]
-    [ProcessorFileTestData(typeof(OpcodeBD))]
-    [ProcessorFileTestData(typeof(OpcodeBE))]
+    [ProcessorFileTestData("a0")]
+    [ProcessorFileTestData("a1")]
+    [ProcessorFileTestData("a2")]
+    [ProcessorFileTestData("a4")]
+    [ProcessorFileTestData("a5")]
+    [ProcessorFileTestData("a6")]
+    [ProcessorFileTestData("a9")]
+    [ProcessorFileTestData("ac")]
+    [ProcessorFileTestData("ad")]
+    [ProcessorFileTestData("ae")]
+    [ProcessorFileTestData("b1")]
+    [ProcessorFileTestData("b4")]
+    [ProcessorFileTestData("b5")]
+    [ProcessorFileTestData("b6")]
+    [ProcessorFileTestData("b9")]
+    [ProcessorFileTestData("bc")]
+    [ProcessorFileTestData("bd")]
+    [ProcessorFileTestData("be")]
     public void Execute10000LoadTestsPerOpcodeCorrectly1(SampleCpu sampleCpu)
     {
         var trace = new List<(int, byte, string)>();
@@ -34,7 +34,7 @@ public class Cpu6502ProcessorMust
     }
 
     [Theory]
-    [ProcessorFileTestData(typeof(OpcodeEA))]
+    [ProcessorFileTestData("ea")]
     public void Execute10000NopTestsPerOpcodeCorrectly(SampleCpu sampleCpu)
     {
         var trace = new List<(int, byte, string)>();
@@ -46,14 +46,14 @@ public class Cpu6502ProcessorMust
     }
 
     [Theory]
-    [ProcessorFileTestData(typeof(Opcode01))]
-    [ProcessorFileTestData(typeof(Opcode05))]
-    [ProcessorFileTestData(typeof(Opcode09))]
-    [ProcessorFileTestData(typeof(Opcode0D))]
-    [ProcessorFileTestData(typeof(Opcode11))]
-    [ProcessorFileTestData(typeof(Opcode15))]
-    [ProcessorFileTestData(typeof(Opcode19))]
-    [ProcessorFileTestData(typeof(Opcode1D))]
+    [ProcessorFileTestData("01")]
+    [ProcessorFileTestData("05")]
+    [ProcessorFileTestData("09")]
+    [ProcessorFileTestData("0d")]
+    [ProcessorFileTestData("11")]
+    [ProcessorFileTestData("15")]
+    [ProcessorFileTestData("19")]
+    [ProcessorFileTestData("1d")]
     public void Execute10000InclusiveOrTestsPerOpcodeCorrectly(SampleCpu sampleCpu)
     {
         var trace = new List<(int, byte, string)>();
@@ -65,10 +65,10 @@ public class Cpu6502ProcessorMust
     }
 
     [Theory]
-    [ProcessorFileTestData(typeof(Opcode25))]
-    [ProcessorFileTestData(typeof(Opcode29))]
-    [ProcessorFileTestData(typeof(Opcode2D))]
-    [ProcessorFileTestData(typeof(Opcode35))]
+    [ProcessorFileTestData("25")]
+    [ProcessorFileTestData("29")]
+    [ProcessorFileTestData("2d")]
+    [ProcessorFileTestData("35")]
     public void Execute10000LogicalAndTestsPerOpcodeCorrectly(SampleCpu sampleCpu)
     {
         var trace = new List<(int, byte, string)>();
--- NesCs.Tests.Common/ProcessorFileTestDataAttribute.cs
+++ NesCs.Tests.Common/ProcessorFileTestDataAttribute.cs
@@ -8,17 +8,11 @@ namespace NesCs.Tests.Common;
 // Based on https://andrewlock.net/creating-a-custom-xunit-theory-test-dataattribute-to-load-data-from-json-files/
 public class ProcessorFileTestDataAttribute : DataAttribute
 {
+    private const string ProcessorTestFilesDirectory = "../../../../../ProcessorTests/nes6502/v1/";
     private readonly string _filename;
 
-    public ProcessorFileTestDataAttribute(Type opcodeFile)
-    {
-        // Based on https://stackoverflow.com/questions/76046475/how-to-invoke-static-interface-method-via-reflection
-        _filename = opcodeFile
-            .GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static)
-            .Where(info => info.Name == "Filename")
-            .SingleOrDefault()
-            ?.GetValue(null) as string ?? throw new ArgumentException($"{opcodeFile.Name} does not implement IOpcodeFile");
-    }
+    public ProcessorFileTestDataAttribute(string opcode) =>
+        _filename = $"{ProcessorTestFilesDirectory}/{opcode}.json";
 
     public override IEnumerable<object[]> GetData(MethodInfo testMethod)
     {
