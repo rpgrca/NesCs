4c6444c28f61d2b1e24caf6296ff0a6adeaee707
Roberto <rpgrca@gmail.com>
2023-07-29 13:50:21 -0300

    refactor, extract indirect y mode as base

--- NesCs.Logic/Cpu/Instructions/LdaInIndirectYModeOpcodeB1.cs
+++ NesCs.Logic/Cpu/Instructions/LdaInIndirectYModeOpcodeB1.cs
@@ -1,28 +1,5 @@
 namespace NesCs.Logic.Cpu.Instructions;
 
-public class LdaInIndirectYModeOpcodeB1 : IInstruction
+public class LdaInIndirectYModeOpcodeB1 : IndirectYMode
 {
-    public void Execute(Cpu6502 cpu)
-    {
-        cpu.ReadyForNextInstruction();
-        var address = cpu.ReadByteFromProgram();
-        var low = cpu.ReadByteFromMemory(address);
-
-        address = (byte)((address + 1) & 0xff);
-        cpu.ReadyForNextInstruction();
-        var high = cpu.ReadByteFromMemory(address);
-
-        var effectiveAddress = (high << 8) | ((low + cpu.ReadByteFromRegisterY()) & 0xff);
-        var a = cpu.ReadByteFromMemory(effectiveAddress);
-
-        var effectiveAddress2 = (((high << 8) | low) + cpu.ReadByteFromRegisterY()) & 0xffff;
-        if (effectiveAddress != effectiveAddress2)
-        {
-            a = cpu.ReadByteFromMemory(effectiveAddress2);
-        }
-
-        cpu.SetValueIntoAccumulator(a);
-        cpu.SetZeroFlagBasedOnAccumulator();
-        cpu.SetNegativeFlagBasedOnAccumulator();
-    }
 }
--- NesCs.Logic/Cpu/Instructions/OraInIndirectYModeOpcode11.cs
+++ NesCs.Logic/Cpu/Instructions/OraInIndirectYModeOpcode11.cs
@@ -1,30 +1,7 @@
 namespace NesCs.Logic.Cpu.Instructions;
 
-public class OraInIndirectYModeOpcode11 : IInstruction
+public class OraInIndirectYModeOpcode11 : IndirectYMode
 {
-    public void Execute(Cpu6502 cpu)
-    {
-        cpu.ReadyForNextInstruction();
-        var address = cpu.ReadByteFromProgram();
-
-        cpu.ReadyForNextInstruction();
-        var low = cpu.ReadByteFromMemory(address);
-        address = (byte)((address + 1) & 0xff);
-        var high = cpu.ReadByteFromMemory(address);
-
-        var effectiveAddress = (high << 8) | ((low + cpu.ReadByteFromRegisterY()) & 0xff);
-        var value = cpu.ReadByteFromMemory(effectiveAddress);
-
-        var effectiveAddress2 = (((high << 8) | low) + cpu.ReadByteFromRegisterY()) & 0xffff;
-        if (effectiveAddress != effectiveAddress2)
-        {
-            value = cpu.ReadByteFromMemory(effectiveAddress2);
-        }
-
-        value |= cpu.ReadByteFromAccumulator();
-        cpu.SetValueIntoAccumulator(value);
-
-        cpu.SetZeroFlagBasedOnAccumulator();
-        cpu.SetNegativeFlagBasedOnAccumulator();
-    }
+    protected override byte ExecuteOperation(Cpu6502 cpu, byte value) =>
+        (byte)(cpu.ReadByteFromAccumulator() | value);
 }
