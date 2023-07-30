b8b7407853b69dea321600cfa89613f30c35d0a4
Roberto <rpgrca@gmail.com>
2023-07-30 19:42:24 -0300

    add test, implement opcode 6d

--- NesCs.Logic/Cpu/Cpu6502Builder.cs
+++ NesCs.Logic/Cpu/Cpu6502Builder.cs
@@ -49,6 +49,7 @@ public partial class Cpu6502
             _instructions[0x3D] = new AndInAbsoluteXModeOpcode3D();
             _instructions[0x58] = new ClearInterruptDisableOpcode58();
             _instructions[0x69] = new AddInImmediateModeOpcode69();
+            _instructions[0x6D] = new AddInAbsoluteModeOpcode6D();
             _instructions[0x78] = new SetInterruptDisableOpcode78();
             _instructions[0x8A] = new TransferXToAccumulatorOpcode8A();
             _instructions[0x98] = new TransferYToAccumulatorOpcode98();
