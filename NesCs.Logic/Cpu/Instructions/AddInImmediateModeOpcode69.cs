489859bdc3e8ef09a2799fdfc970b59ff622e199
Roberto <rpgrca@gmail.com>
2023-07-30 18:53:21 -0300

    extract base for add and sub in immediate

--- NesCs.Logic/Cpu/Instructions/AddInImmediateModeOpcode69.cs
+++ NesCs.Logic/Cpu/Instructions/AddInImmediateModeOpcode69.cs
@@ -1,36 +1,7 @@
 namespace NesCs.Logic.Cpu.Instructions;
 
-public class AddInImmediateModeOpcode69 : IInstruction
+public class AddInImmediateModeOpcode69 : MathImmediateMode
 {
-    public void Execute(Cpu6502 cpu)
-    {
-        cpu.ReadyForNextInstruction();
-        var value = cpu.ReadByteFromProgram();
-
-        cpu.ReadyForNextInstruction();
-        var a = cpu.ReadByteFromAccumulator();
-
-        var sum = a + value + (cpu.ReadCarryFlag() == Cpu6502.ProcessorStatus.C? 1 : 0);
-        var result = (byte)(sum & 0xff);
-
-        cpu.SetValueIntoAccumulator(result);
-        cpu.ClearCarryFlag();
-        cpu.ClearNegativeFlag();
-        cpu.ClearOverflowFlag();
-        cpu.ClearZeroFlag();
-
-        // TODO: Not a real 8-bit implementation but works for the time being
-        if ((sum >> 8) != 0)
-        {
-            cpu.SetCarryFlag();
-        }
-
-        if (((a ^ result) & (value ^ result) & 0x80) != 0)
-        {
-            cpu.SetOverflowFlag();
-        }
-
-        cpu.SetZeroFlagBasedOn(result);
-        cpu.SetNegativeFlagBasedOn(result);
-    }
+    protected override (int, byte) ExecuteOperation(Cpu6502 cpu, byte accumulator, byte value) =>
+        (accumulator + value + (cpu.ReadCarryFlag() == Cpu6502.ProcessorStatus.C? 1 : 0), value);
 }
--- NesCs.Logic/Cpu/Instructions/SubtractInImmediateModeOpcodeE9.cs
+++ NesCs.Logic/Cpu/Instructions/SubtractInImmediateModeOpcodeE9.cs
@@ -2,64 +2,11 @@ using static NesCs.Logic.Cpu.Cpu6502;
 
 namespace NesCs.Logic.Cpu.Instructions;
 
-public class SubtractInImmediateModeOpcodeE9 : IInstruction
+public class SubtractInImmediateModeOpcodeE9 : MathImmediateMode
 {
-    public void Execute(Cpu6502 cpu)
+    protected override (int, byte) ExecuteOperation(Cpu6502 cpu, byte accumulator, byte value)
     {
-        cpu.ReadyForNextInstruction();
-        var value = cpu.ReadByteFromProgram();
-
-        cpu.ReadyForNextInstruction();
-        var a = cpu.ReadByteFromAccumulator();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         value = (byte)~value;
-        var sum = a + value + (cpu.ReadCarryFlag() == ProcessorStatus.C? 1 : 0);
-        var result = (byte)(sum & 0xff);
-
-        cpu.SetValueIntoAccumulator(result);
-        cpu.ClearCarryFlag();
-        cpu.ClearNegativeFlag();
-        cpu.ClearOverflowFlag();
-        cpu.ClearZeroFlag();
-
-        // TODO: Not a real 8-bit implementation but works for the time being
-        if ((sum >> 8) != 0)
-        {
-            cpu.SetCarryFlag();
-        }
-
-        if (((a ^ result) & (value ^ result) & 0x80) != 0)
-        {
-            cpu.SetOverflowFlag();
-        }
-
-        cpu.SetZeroFlagBasedOn(result);
-        cpu.SetNegativeFlagBasedOn(result);
+        return (accumulator + value + (cpu.ReadCarryFlag() == ProcessorStatus.C? 1 : 0), value);
     }
 }
