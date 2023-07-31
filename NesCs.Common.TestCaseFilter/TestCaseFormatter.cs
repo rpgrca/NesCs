cb3ac0c3694e719a9ee02ba07882f3093fa2b09d
Roberto <rpgrca@gmail.com>
2023-07-30 22:17:06 -0300

    fix format for comment

--- NesCs.Common.TestCaseFilter/TestCaseFormatter.cs
+++ NesCs.Common.TestCaseFilter/TestCaseFormatter.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using System.Text;
+using System.Numerics;
 
 using NesCs.Logic.Cpu;
 
@@ -15,13 +16,22 @@ public class TestCaseFormatter
         _builder.AppendLine($"    public static IEnumerable<object[]> Opcode{opcode}JsonFeeder()");
         _builder.AppendLine("    {");
 
+        var max = cases.Keys.Max(p => BitOperations.PopCount((uint)p));
+        var formatNumber = $"/* {{0,3}} ";
+        var formatFlags = $"{{0,{-max}}} */ ";
+
         foreach (var value in cases.OrderBy(p => p.Key))
         {
             var ps = value.Key == 0
                 ? ""
                 : new string(((Cpu6502.ProcessorStatus)value.Key).ToString().Replace(" ", "").Replace(",", "").Reverse().ToArray());
 
-            _builder.AppendLine($$""""        /* {{value.Key,3}} {{ps,-4}} */ yield return new object[] { JsonDeserializer.Deserialize("""{{value.Value}}""") };"""");
+            _builder.Append("        ");
+            _builder.Append(string.Format(formatNumber, value.Key));
+            _builder.Append(string.Format(formatFlags, ps));
+            _builder.Append("yield return new object[] { JsonDeserializer.Deserialize(\"\"\"");
+            _builder.Append(value.Value);
+            _builder.AppendLine("\"\"\") };");
         }
 
         _builder.AppendLine("    }");
