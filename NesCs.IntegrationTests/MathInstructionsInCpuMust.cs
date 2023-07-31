72fb2cca18173fd9f10f0bc6a9b6b0631c4e5440
Roberto <rpgrca@gmail.com>
2023-07-31 13:34:20 -0300

    add test, implement opcode e4

--- NesCs.IntegrationTests/MathInstructionsInCpuMust.cs
+++ NesCs.IntegrationTests/MathInstructionsInCpuMust.cs
@@ -44,6 +44,7 @@ public class MathInstructionsInCpuMust
 
     [Theory]
     [ProcessorFileTestData("e0")]
+    [ProcessorFileTestData("e4")]
     [ProcessorFileTestData("ec")]
     public void Execute10000CompareTestsPerOpcodeCorrectly(SampleCpu sampleCpu)
     {
