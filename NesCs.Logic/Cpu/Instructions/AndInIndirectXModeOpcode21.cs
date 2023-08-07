22d6b52310f031a72ae85b628777175883ce278b
Roberto <rpgrca@gmail.com>
2023-08-07 17:00:14 -0300

    implement and using composition, remove unused classes

--- NesCs.Logic/Cpu/Instructions/AndInIndirectXModeOpcode21.cs
+++
@@ -1,9 +0,0 @@
-using NesCs.Logic.Cpu.Instructions.Modes;
-
-namespace NesCs.Logic.Cpu.Instructions;
-
-public class AndInIndirectXModeOpcode21 : IndirectXMode
-{
-    protected override byte ExecuteOperation(Cpu6502 cpu, byte value) =>
-        (byte)(value & cpu.ReadByteFromAccumulator());
-}
