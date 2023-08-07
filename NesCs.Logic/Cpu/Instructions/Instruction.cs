dbc8b967556bfd547bdb23fb22f161f7edcb869d
Roberto <rpgrca@gmail.com>
2023-08-07 00:48:04 -0300

    use simpler Instruction class, delete repeated adc ones

--- NesCs.Logic/Cpu/Instructions/Instruction.cs
+++ NesCs.Logic/Cpu/Instructions/Instruction.cs
@@ -1,20 +1,24 @@
-using NesCs.Logic.Cpu.Operations;
 using NesCs.Logic.Cpu.Addressings;
+using NesCs.Logic.Cpu.Operations;
 
 namespace NesCs.Logic.Cpu.Instructions;
 
-public class AddInAbsoluteModeOpcode6D : IInstruction
+public class Instruction : IInstruction
 {
+    public readonly byte _opcode;
+    private readonly string _name;
     private readonly IAddressing _addressing;
     private readonly IOperation _operation;
 
-    public AddInAbsoluteModeOpcode6D(IAddressing addressing, IOperation operation)
+    public Instruction(byte opcode, string name, IAddressing addressing, IOperation operation)
     {
+        _opcode = opcode;
+        _name = name;
         _addressing = addressing;
         _operation = operation;
     }
 
-    public void Execute(Cpu6502 cpu)
+    void IInstruction.Execute(Cpu6502 cpu)
     {
         var value = _addressing.ObtainValue(cpu);
         _operation.Execute(cpu, value);
