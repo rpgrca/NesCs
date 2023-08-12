86bb1df5e0595edf871cc7344b9cc44e74220614
Roberto <rpgrca@gmail.com>
2023-08-11 23:52:41 -0300

    add test, vram address increment

--- NesCs.Logic/ControlRegister.cs
+++ NesCs.Logic/ControlRegister.cs
@@ -4,9 +4,17 @@ public class ControlRegister
 {
     private byte _flags;
 
-    public void SetBaseNametableAddress(byte value) =>
-        _flags |= (byte)(value & 0b11);
+    public byte N
+    {
+        get => (byte)(_flags & 0b11);
+        set => _flags |= (byte)(value & 0b11);
+    }
 
-    public int GetBaseNametableAddress() =>
-        0x2000 + (_flags & 0b11) * 0x400;
+    public byte I
+    {
+        get => (byte)((_flags & 0b100) >> 2);
+        set => _flags |= (byte)((value & 1) << 2);
+    }
+
+    public int GetBaseNametableAddress() => 0x2000 + (_flags & 0b11) * 0x400;
 }
--- NesCs.UnitTests/Ppu/ControlRegisterMust.cs
+++ NesCs.UnitTests/Ppu/ControlRegisterMust.cs
@@ -3,14 +3,43 @@ namespace NesCs.UnitTests.Ppu;
 public class ControlRegisterMust
 {
     [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    public void SetBaseNametableAddressBitsCorrectly(byte value)
+    {
+        var sut = new Logic.Ppu.ControlRegister
+        {
+            N = value
+        };
+
+        Assert.Equal(value, sut.N);
+    }
+
+    [Theory]
     [InlineData(0, 0x2000)]
     [InlineData(1, 0x2400)]
     [InlineData(2, 0x2800)]
     [InlineData(3, 0x2C00)]
     public void CalculateBaseNametableAddressCorrectly(byte value, int expectedAddress)
     {
-        var sut = new Logic.Ppu.ControlRegister();
-        sut.SetBaseNametableAddress(value);
+        var sut = new Logic.Ppu.ControlRegister
+        {
+            N = value
+        };
         Assert.Equal(expectedAddress, sut.GetBaseNametableAddress());
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    public void SetVramAddressIncrementCorrectly(byte value)
+    {
+        var sut = new Logic.Ppu.ControlRegister
+        {
+            I = value
+        };
+        Assert.Equal(value, sut.I);
     }
 }
