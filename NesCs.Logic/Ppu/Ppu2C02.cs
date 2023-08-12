0ab73020004f5f2d951033c448248deb2e87a503
Roberto <rpgrca@gmail.com>
2023-08-11 22:32:53 -0300

    initial ppu, empty

---
+++ NesCs.Logic/Ppu/Ppu2C02.cs
@@ -0,0 +1,25 @@
+namespace NesCs.Logic.Ppu;
+
+public class Ppu2C02
+{
+    public Controller PpuCtrl { get; }
+    public Mask PpuMask { get; }
+    public Status PpuStatus { get; }
+    public ObjectAttributeMemory OamData { get; }
+    public Scroll PpuScroll { get; }
+    public Address PpuAddr { get; }
+    public Data PpuData { get; }
+    public ObjectAttributeMemory OamDma { get; }
+
+    public Ppu2C02()
+    {
+        PpuCtrl = new Controller();
+        PpuMask = new Mask();
+        PpuStatus = new Status();
+        OamData = new ObjectAttributeMemory();
+        PpuScroll = new Scroll();
+        PpuAddr = new Address();
+        PpuData = new Data();
+        OamDma = new ObjectAttributeMemory();
+    }
+}
---
+++ NesCs.UnitTests/Ppu/PpuRamMust.cs
@@ -0,0 +1,18 @@
+using NesCs.Logic.Ppu;
+
+public class Ppu2C02Must
+{
+    [Fact]
+    public void BeInitializedCorrectly()
+    {
+        var sut = new Ppu2C02();
+        Assert.NotNull(sut.PpuCtrl);
+        Assert.NotNull(sut.PpuMask);
+        Assert.NotNull(sut.PpuStatus);
+        Assert.NotNull(sut.OamData);
+        Assert.NotNull(sut.PpuScroll);
+        Assert.NotNull(sut.PpuAddr);
+        Assert.NotNull(sut.PpuData);
+        Assert.NotNull(sut.OamDma);
+    }
+}
