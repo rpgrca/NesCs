d8833545df04ea1dac05b3cc2f80a6f401ce9b0c
Roberto <rpgrca@gmail.com>
2023-08-12 22:53:32 -0300

    add test, implement open bus in status

--- NesCs.Logic/Ppu/Status.cs
+++ NesCs.Logic/Ppu/Status.cs
@@ -2,5 +2,11 @@ namespace NesCs.Logic.Ppu;
 
 public class Status
 {
+    private byte _flags;
 
+    public byte OpenBus
+    {
+        get => (byte)(_flags & 0b11111);
+        set => _flags |= (byte)(value & 0b11111);
+    }
 }
---
+++ NesCs.UnitTests/Ppu/StatusMust.cs
@@ -0,0 +1,19 @@
+namespace NesCs.UnitTests.Ppu;
+
+public class StatusMust
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(0b11111100, 0b11100)]
+    [InlineData(0b11110000, 0b10000)]
+    public void SetOpenBusCorrectly(byte value, byte expectedValue)
+    {
+        var sut = new Logic.Ppu.Status
+        {
+            OpenBus = value
+        };
+
+        Assert.Equal(expectedValue, sut.OpenBus);
+    }
+}
