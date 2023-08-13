14daff86b5cb280c002e58c7d9b41d78e7f643c5
Roberto <rpgrca@gmail.com>
2023-08-13 00:01:50 -0300

    add test, consecutive writes set y camera position correctly

--- NesCs.Logic/Ppu/ScrollingPositionRegister.cs
+++ NesCs.Logic/Ppu/ScrollingPositionRegister.cs
@@ -2,5 +2,19 @@ namespace NesCs.Logic.Ppu;
 
 public class ScrollingPositionRegister
 {
-    public byte Position { get; set; }
+    private byte _index = 0;
+    private byte[] _cameraPosition = { 0, 0 };
+
+    public byte CameraPositionX => _cameraPosition[0];
+    public byte CameraPositionY => _cameraPosition[1];
+
+
+    public byte Position {
+        private get => 0;
+        set
+        {
+            _cameraPosition[_index] = value;
+            _index = (byte)((_index + 1) & 1);
+        }
+    }
 }
