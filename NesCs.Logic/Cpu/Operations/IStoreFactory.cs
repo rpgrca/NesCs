64867c0e04139f9abc4c6e3f14eae732bb092d58
Roberto <rpgrca@gmail.com>
2023-08-17 01:27:44 -0300

    remove unused methods

--- NesCs.Logic/Cpu/Operations/IStoreFactory.cs
+++ NesCs.Logic/Cpu/Operations/IStoreFactory.cs
@@ -2,9 +2,6 @@ namespace NesCs.Logic.Cpu.Operations;
 
 public interface IStoreFactory
 {
-    IOperation X { get; }
-    IOperation Y { get; }
-    IOperation Accumulator { get; }
     IOperation Memory { get; }
     IOperation Stack { get; }
 }
--- NesCs.Logic/Cpu/Operations/StoreFactory.cs
+++ NesCs.Logic/Cpu/Operations/StoreFactory.cs
@@ -2,12 +2,6 @@ namespace NesCs.Logic.Cpu.Operations;
 
 public class StoreFactory : IStoreFactory
 {
-    public IOperation X { get; } = new Store((c, _, v) => c.SetValueToRegisterX(v));
-
-    public IOperation Y { get; } = new Store((c, _, v) => c.SetValueToRegisterY(v));
-
-    public IOperation Accumulator { get; } = new Store((c, _, v) => c.SetValueToAccumulator(v));
-
     public IOperation Memory { get; } = new Store((c, a, v) => c.WriteByteToMemory(a, v));
 
     public IOperation Stack { get; } = new Store((c, _, v) => c.SetValueToStackPointer(v));
