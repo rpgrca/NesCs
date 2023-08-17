427c15a8b583774cbea5be15511aa575936c949a
Roberto <rpgrca@gmail.com>
2023-08-17 01:31:06 -0300

    remove default implementation

--- NesCs.Logic/Cpu/Addressings/IAddressing.cs
+++ NesCs.Logic/Cpu/Addressings/IAddressing.cs
@@ -2,6 +2,6 @@ namespace NesCs.Logic.Cpu.Addressings;
 
 public interface IAddressing
 {
-    (int Address, byte Value) ObtainValueAndAddress(Cpu6502 cpu) => throw new NotImplementedException();
+    (int Address, byte Value) ObtainValueAndAddress(Cpu6502 cpu);
     byte[] PeekOperands(Cpu6502 cpu);
 }
