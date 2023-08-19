b6e3529791cf3eb5bbe0647a247e4518a060872c
Roberto <rpgrca@gmail.com>
2023-08-19 15:48:14 -0300

    readjust power off addresses

--- NesCs.Roms.IntegrationTests/InstrTestv5Must.cs
+++ NesCs.Roms.IntegrationTests/InstrTestv5Must.cs
@@ -8,22 +8,22 @@ namespace NesCs.Roms.IntegrationTests;
 public class InstrTestv5Must
 {
     [Theory]
-    [InlineData("instr_test-v5/rom_singles/01-basics.nes", 0xE976, "\n01-basics\n\nPassed\n")]
+    [InlineData("instr_test-v5/rom_singles/01-basics.nes", 0xE8D5, "\n01-basics\n\nPassed\n")]
     [InlineData("instr_test-v5/rom_singles/02-implied.nes", 0xE976, "\n02-implied\n\nPassed\n")]
-    [InlineData("instr_test-v5/rom_singles/03-immediate.nes", 0xE976, "\n03-implied\n\nPassed\n", Skip = "Must implement 0x0B")]
+    [InlineData("instr_test-v5/rom_singles/03-immediate.nes", 0xE8D5, "\n03-implied\n\nPassed\n", Skip = "Must implement 0x0B")]
     [InlineData("instr_test-v5/rom_singles/04-zero_page.nes", 0xE976, "\n04-zero_page\n\nPassed\n")]
     [InlineData("instr_test-v5/rom_singles/05-zp_xy.nes", 0xE976, "\n05-zp_xy\n\nPassed\n")]
     [InlineData("instr_test-v5/rom_singles/06-absolute.nes", 0xE976, "\n06-absolute\n\nPassed\n")]
-    [InlineData("instr_test-v5/rom_singles/07-abs_xy.nes", 0xE976, "", Skip = "Must implement 0x9C?")]
+    [InlineData("instr_test-v5/rom_singles/07-abs_xy.nes", 0xE8D5, "", Skip = "Must implement 0x9C?")]
     [InlineData("instr_test-v5/rom_singles/08-ind_x.nes", 0xE976, "\n08-ind_x\n\nPassed\n")]
-    [InlineData("instr_test-v5/rom_singles/09-ind_y.nes", 0xE876, "\n09-ind_y\n\nPassed\n")]
+    [InlineData("instr_test-v5/rom_singles/09-ind_y.nes", 0xE976, "\n09-ind_y\n\nPassed\n")]
     [InlineData("instr_test-v5/rom_singles/10-branches.nes", 0xE876, "\n10-branches\n\nPassed\n")]
-    [InlineData("instr_test-v5/rom_singles/11-stack.nes", 0xE876, "\n11-stack\n\nPassed\n")]
+    [InlineData("instr_test-v5/rom_singles/11-stack.nes", 0xE976, "\n11-stack\n\nPassed\n")]
     [InlineData("instr_test-v5/rom_singles/12-jmp_jsr.nes", 0xE876, "\n12-jmp_jsr\n\nPassed\n")]
     [InlineData("instr_test-v5/rom_singles/13-rts.nes", 0xE876, "\n13-rts\n\nPassed\n")]
     [InlineData("instr_test-v5/rom_singles/14-rti.nes", 0xE876, "\n14-rti\n\nPassed\n")]
     [InlineData("instr_test-v5/rom_singles/15-brk.nes", 0xE876, "\n15-brk\n\nPassed\n")]
-    [InlineData("instr_test-v5/rom_singles/16-special.nes", 0xE8D5, "\n16-special\n\nPassed\n")]
+    [InlineData("instr_test-v5/rom_singles/16-special.nes", 0xE7D5, "\n16-special\n\nPassed\n")]
     public void ReturnPassed(string romName, int poweroffAddress, string expectedResult)
     {
         var ram = new byte[0x10000];
@@ -37,7 +37,7 @@ public class InstrTestv5Must
 
         var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
         var ramController = new RamController.Builder().WithRamOf(ram).Build();
-        var ppu = new Ppu2C02(ramController);
+        var ppu = new Ppu2C02.Builder().WithRamController(ramController).Build();
 
         var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
         var cpu = builder
