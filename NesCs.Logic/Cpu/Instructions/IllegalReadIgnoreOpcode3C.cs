48c5c44e3a18e3eba37e5e5252d438b3b065fbe4
Roberto <rpgrca@gmail.com>
2023-08-31 10:32:16 -0300

    inherit 1c for 3c to pass tests

--- NesCs.Logic/Cpu/Instructions/IllegalReadIgnoreOpcode3C.cs
+++ NesCs.Logic/Cpu/Instructions/IllegalReadIgnoreOpcode3C.cs
@@ -2,41 +2,9 @@ using NesCs.Logic.Cpu.Instructions;
 
 namespace NesCs.Logic.Cpu;
 
-internal class IllegalReadIgnoreOpcode3C : IInstruction
+internal class IllegalReadIgnoreOpcode3C : IllegalReadIgnoreOpcode1C
 {
-    public string Name => "IGN";
+    public override string Name => "IGN";
 
-    public byte Opcode => 0x3C;
-
-    string IDebuggerDisplay.Display => $"{Opcode:X2} {Name} (abx)";
-
-    public byte[] PeekOperands(Cpu6502 cpu)
-    {
-        byte[] operands = { cpu.PeekMemory(cpu.ReadByteFromProgramCounter() + 1), cpu.PeekMemory(cpu.ReadByteFromProgramCounter() + 2) };
-        return operands;
-    }
-
-    public void Execute(Cpu6502 cpu)
-    {
-        cpu.ReadyForNextInstruction();
-        var low = cpu.ReadByteFromProgram();
-
-        cpu.ReadyForNextInstruction();
-        var high = cpu.ReadByteFromProgram();
-
-        int address = 0;
-
-        address = high << 8 | (low + cpu.ReadByteFromRegisterX()) & 0xff;
-        _ = cpu.ReadByteFromMemory(address);
-
-        if (low + cpu.ReadByteFromRegisterX() > 255)
-        {
-            address = (high << 8 | low) + cpu.ReadByteFromRegisterX();
-        }
-
-        _ = cpu.ReadByteFromMemory(address);
-
-        cpu.ReadyForNextInstruction();
-    }
-
+    public override byte Opcode => 0x3C;
 }
