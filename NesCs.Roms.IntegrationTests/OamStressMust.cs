591f946853164e2960fb42bee9d64acc58720ec4
Roberto <rpgrca@gmail.com>
2023-09-03 00:19:01 -0300

    add some more samples that still dont work

---
+++ NesCs.Roms.IntegrationTests/OamStressMust.cs
@@ -0,0 +1,53 @@
+using NesCs.Logic.Cpu;
+using NesCs.Logic.Ppu;
+using NesCs.Logic.Ram;
+using NesCs.Logic.File;
+using NesCs.Logic.Cpu.Clocking;
+using NesCs.Logic.Tracing;
+
+namespace NesCs.Roms.IntegrationTests;
+
+public class OamStressMust
+{
+    [Theory]
+    [InlineData("oam_stress/oam_stress.nes", 0xE755, "----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n\noam_read\n\nPassed\n", Skip = "wip")]
+    [InlineData("instr_timing/rom_singles/1-instr_timing.nes", 0x1, "", Skip = "wip")]
+    [InlineData("branch_timing_tests/1.Branch_Basics.nes", 0x1, "", Skip = "wip")]
+    public void BeExecutedCorrectly(string romName, int poweroffAddress, string expectedResult)
+    {
+        var ram = new byte[0x10000];
+        var fsp = new FileSystemProxy.Builder().Loading(new NesFileOptions
+        {
+            LoadHeader = true,
+            LoadTrainer = true,
+            LoadProgramRom = true,
+            LoadCharacterRom = true
+        }).Build();
+
+        var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
+        var clock = new Clock(0);
+        var ramController = new RamController.Builder().WithRamOf(ram).PreventRomRewriting().Build();
+        var ppu = new Ppu2C02.Builder().WithRamController(ramController).WithClock(clock).Build();
+        ramController.RegisterHook(ppu);
+
+        var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
+        var cpu = builder
+            .Running(nesFile.ProgramRom)
+            .WithClock(clock)
+            .SupportingInvalidInstructions()
+            .WithRamController(ramController)
+            .WithCallback(poweroffAddress, (cpu, _) => cpu.Stop())
+            .TracingWith(new Vm6502DebuggerDisplay())
+            .Build();
+
+        cpu.PowerOn();
+        cpu.Run();
+
+        var result = GetString(ram);
+        Assert.Equal(0, ram[0x6000]);
+        Assert.Equal(expectedResult, result);
+    }
+
+    private static string GetString(byte[] ram) =>
+        System.Text.Encoding.ASCII.GetString(ram[0x6004..].TakeWhile(p => !p.Equals(0)).ToArray());
+}
