e61c979e456095af1e554545e4d5c205f8e0a30c
Roberto <rpgrca@gmail.com>
2023-09-03 18:59:08 -0300

    initial ppu frame timing sample

--- NesCs.Logic/Ppu/Ppu2C02.cs
+++ NesCs.Logic/Ppu/Ppu2C02.cs
@@ -8,10 +8,7 @@ namespace NesCs.Logic.Ppu;
 public class Ppu2C02 : IPpu
 {
     // TODO: Should only skip, not change cycle amount
-    private const int EvenCycle = 341;
-    private const int OddCycle = 340;
     private const int LinesPerSync = 262;
-    private static readonly int[] _cyclesPerLine = { EvenCycle, OddCycle };
 
     public class Builder
     {
@@ -52,10 +49,9 @@ public class Ppu2C02 : IPpu
     private readonly OamSprite[] _secondaryOam;
     private readonly IByteToggle _toggle;
     private readonly IPpuIOBus _ioBus;
-    private int _currentCycle;
+    private bool _odd;
 
     private RasterAddress Raster { get; }
-
     public ControlRegister PpuCtrl { get; }                 /* 0x2000 W  */
     public Mask PpuMask { get; }                            /* 0x2001 W  */
     public Status PpuStatus { get; }                        /* 0x2002  R */
@@ -74,7 +70,7 @@ public class Ppu2C02 : IPpu
         _toggle = new ByteToggle();
         _ioBus = new PpuIOBus(clock);
         clock.AddPpu(this);
-        _currentCycle = 0;
+        _odd = true;
 
         Raster = new RasterAddress();
         PpuCtrl = new ControlRegister(ram, _ioBus);
@@ -136,40 +132,113 @@ public class Ppu2C02 : IPpu
 
     byte IPpuVram.Read() => _vram[CurrentAddress];
 
+    private int _cycle;
+//    private StringBuilder _line = new StringBuilder();
+/*
+    private List<IPpuLineStep>[] _odd = new List<IPpuLineStep>
+    {
+        new Skip(),
+        new LoadNametableByteCycle1(),
+        new LoadNametableByteCycle2(),
+        new LoadAttributeByteCycle1(),
+        new LoadAttributeByteCycle2(),
+        new LoadLowBackgroundTileByte(),
+        new LoadHighBackgroundTileByteAndIncrementHorizontal()
+    }
+
+    private List<IPpuLineStep>[] _even = new()
+    {
+
+    }
+*/
     public bool Trigger(IClock clock)
     {
+//        string value;
         if (clock.GetCycles() % 4 == 0)
         {
-            Raster.IncrementX();
+            switch (Raster.X)
+            {
+                case 0:
+//                    value = " idl |";
 
-            if (Raster.X >= _cyclesPerLine[_currentCycle])
-            {
-                Raster.ResetX();
-                Raster.IncrementY();
-            }
-            else
-            {
-                if (Raster.X == 1)
-                {
+                    if (Raster.Y == 0)
+                    {
+                        _odd = !_odd;
+
+                        if (!_odd)
+                        {
+//                            value = " skp |";
+                        }
+                    }
+
+                    //_line.Append(value);
+                    Raster.IncrementX();
+                    break;
+
+                case 1:
+//                    value = "     |";
                     if (Raster.Y == 241)
                     {
                         PpuStatus.V = 1;
+                        _cycle = clock.GetCycles();
+//                        value = " +vb |";
                     }
                     else
                     {
                         if (Raster.Y == 261)
                         {
                             PpuStatus.V = 0;
+                            PpuStatus.S = 0;
+                            PpuStatus.O = 0;
+                            _cycle = clock.GetCycles() - _cycle;
+//                            value = " -vb |";
                         }
                     }
-                }
+
+//                    _line.Append(value);
+                    Raster.IncrementX();
+                    break;
+
+                case 339:
+//                    _line.Append("     |");
+
+                    if (Raster.Y == 261 && _odd)
+                    {
+                        Raster.ResetX();
+                        Raster.ResetY();
+//                        _line.Append("\n\n\n");
+//                        System.IO.File.AppendAllText("/home/roberto/src/NesCs/ppu.log", _line.ToString());
+//                        _line.Clear();
+                    }
+                    else
+                    {
+                        Raster.IncrementX();
+                    }
+                    break;
+
+                case 340:
+//                    _line.Append("     |\n");
+
+                    Raster.ResetX();
+                    Raster.IncrementY();
+                    if (Raster.Y >= LinesPerSync)
+                    {
+                        Raster.ResetY();
+//                        _line.Append("\n\n\n");
+                    }
+
+//                    System.IO.File.AppendAllText("/home/roberto/src/NesCs/ppu.log", _line.ToString());
+//                    _line.Clear();
+                    break;
+
+                default:
+//                    _line.Append("     |");
+                    Raster.IncrementX();
+                    break;
             }
 
-            if (Raster.Y >= LinesPerSync)
-            {
-                Raster.ResetY();
-                _currentCycle = (_currentCycle + 1) % 2;
-            }
+            return true;
+
 
 //                // On an NTSC machine, the VBL flag is cleared 6820 ppu cycles or exactly 20 scanlines after it is set.
 //                if (Raster.Y == 20 && Raster.X == 0)
@@ -208,4 +277,75 @@ public class Ppu2C02 : IPpu
 
     [DebuggerBrowsable(DebuggerBrowsableState.Never)]
     private string DebuggerDisplay => $"PPU: {Raster.Y,3},{Raster.X,3}";
+}
+
+public interface IPpuLineStep
+{
+    void Work(Ppu2C02 ppu);
+}
+
+public class Idle : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+    }
+}
+
+public class Skip : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+    }
+}
+
+public class LoadNametableByteCycle1 : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+    }
+}
+
+public class LoadNametableByteCycle2 : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+    }
+}
+
+public class LoadAttributeByteCycle1 : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+    }
+}
+
+public class LoadAttributeByteCycle2 : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+    }
+}
+
+public class LoadLowBackgroundTileByte : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class LoadHighBackgroundTileByteAndIncrementHorizontal : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class IncrementHorizontal : IPpuLineStep
+{
+    public void Work(Ppu2C02 ppu)
+    {
+        throw new NotImplementedException();
+    }
 }
