9778e02b38e6f9db222b1ee5a41fe7f40eaefa17
Roberto <rpgrca@gmail.com>
2023-09-10 20:56:59 -0300

    update error message

--- NesCs.Roms.IntegrationTests/InstrMiscMust.cs
+++ NesCs.Roms.IntegrationTests/InstrMiscMust.cs
@@ -11,7 +11,7 @@ public class InstrMiscMust
     [Theory]
     [InlineData("instr_misc/rom_singles/01-abs_x_wrap.nes", 0xE7B5, "\n01-abs_x_wrap\n\nPassed\n")] // ticks: 786229
     [InlineData("instr_misc/rom_singles/02-branch_wrap.nes", 0xE7B5, "\n02-branch_wrap\n\nPassed\n")] // ticks: 793369
-    [InlineData("instr_misc/rom_singles/03-dummy_reads.nes", 0xE7A8, "", Skip = "not working")]
+    [InlineData("instr_misc/rom_singles/03-dummy_reads.nes", 0x1, "", Skip = "\nSTA abs,x\n\n03-dummy_reads\n\nFailed #4\n")]
     public void ReturnPassed(string romName, int poweroffAddress, string expectedResult)
     {
         var clock = new Clock(0);
@@ -27,8 +27,14 @@ public class InstrMiscMust
         var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
         var ramController = new RamController.Builder().WithRamOf(ram).Build();
         var ppu = new Ppu2C02.Builder().WithRamController(ramController).WithClock(clock).Build();
+        ramController.RegisterHook(ppu);
 
         var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
+        if (nesFile.ProgramRomSize == 1)
+        {
+            builder.ProgramMappedAt(0xC000); // NROM-128
+        }
+
         var cpu = builder
             .Running(nesFile.ProgramRom)
             .SupportingInvalidInstructions()
