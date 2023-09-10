e26cad5139755f608dfb80a27e4f9fb221001e51
Roberto <rpgrca@gmail.com>
2023-09-10 18:42:58 -0300

    add extra information about apu failure

--- NesCs.Roms.IntegrationTests/CpuExecSpaceMust.cs
+++ NesCs.Roms.IntegrationTests/CpuExecSpaceMust.cs
@@ -10,7 +10,7 @@ public class CpuExecSpaceMust
 {
     [Theory]
     [InlineData("cpu_exec_space/test_cpu_exec_space_ppuio.nes", 0xE7F5, "\u001b[0;37mTEST:test_cpu_exec_space_ppuio\n\u001b[0;33mThis program verifies that the\nCPU can execute code from any\npossible location that it can\naddress, including I/O space.\n\nIn addition, it will be tested\nthat an RTS instruction does a\ndummy read of the byte that\nimmediately follows the\ninstructions.\n\n\u001b[0;37m\u001b[1;34mJSR+RTS TEST OK\nJMP+RTS TEST OK\nRTS+RTS TEST OK\nJMP+RTI TEST OK\nJMP+BRK TEST OK\n\u001b[0;37m\nPassed\n")] // ticks: 3293317
-    [InlineData("cpu_exec_space/test_cpu_exec_space_apu.nes", 0xE976, "", Skip = "need to implement apu")]
+    [InlineData("cpu_exec_space/test_cpu_exec_space_apu.nes", 0xE976, "", Skip = "Must implement APU, landed at $0234, fail to obey predetermined path #2")]
     public void BeExecutedCorrectly(string romName, int poweroffAddress, string expectedResult)
     {
         var ram = new byte[0x10000];
