6efefe4f73d1207c85689529915c257f211365e9
Roberto <rpgrca@gmail.com>
2023-09-11 00:15:01 -0300

    add debugger info for mapper

--- NesCs.Logic/File/Mapper.cs
+++ NesCs.Logic/File/Mapper.cs
@@ -1,8 +1,13 @@
+using System.Diagnostics;
+
 namespace NesCs.Logic.File;
 
+[DebuggerDisplay("{DebuggerDisplay}")]
 public struct Mapper
 {
     public int Number { get; init; }
     public int StartAddress { get; init; }
     public int EndAddress { get; init; }
+
+    private readonly string DebuggerDisplay => $"Mapper {Number} ({StartAddress:X4}-{EndAddress:X4})";
 }
