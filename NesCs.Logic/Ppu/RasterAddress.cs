bdc5edc0eef348fe6225f052654efd51ebcf3027
Roberto <rpgrca@gmail.com>
2023-09-16 00:07:22 -0300

    add methods for ignoring vblank in raster

--- NesCs.Logic/Ppu/RasterAddress.cs
+++ NesCs.Logic/Ppu/RasterAddress.cs
@@ -7,35 +7,26 @@ public class RasterAddress
 {
     public int X { get; private set; }
     public int Y { get; private set; }
+    public bool IgnoringVblank { get; private set; }
 
     public RasterAddress() => X = Y = 0;
-/*
-    public void IncrementX()
-    {
-        X += 1;
-        if (X > 340)
-        {
-            X = 0;
-            Y += 1;
-
-            if (Y > 261)
-            {
-                Y = 0;
-            }
-        }
-    }*/
 
     internal void BackToOrigin()
     {
         Y = 0;
         X = 0;
+        IgnoringVblank = false;
     }
 
     internal void ResetX() => X = 0;
 
-    internal void ResetY() => Y = 0;
-
     internal void IncrementX() => X++;
 
     internal void IncrementY() => Y++;
+
+    internal bool IsVblSetNextCycle() => Y == 241 && X == 0;
+
+    internal bool IsVblJustSet() => Y == 241 && (X == 1 || X == 2);
+
+    internal void IgnoreVblankThisFrame() => IgnoringVblank = true;
 }
