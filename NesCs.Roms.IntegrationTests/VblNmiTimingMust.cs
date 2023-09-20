f05677e77a162f46fa219fde4c65399fd2a876df
Roberto <rpgrca@gmail.com>
2023-09-19 23:45:45 -0300

    add expected output

--- NesCs.Roms.IntegrationTests/VblNmiTimingMust.cs
+++ NesCs.Roms.IntegrationTests/VblNmiTimingMust.cs
@@ -12,12 +12,12 @@ public class VblNmiTimingMust
 {
     [Theory]
     [InlineData("vbl_nmi_timing/1.frame_basics.nes", 0xE589, 0xE217, "PPU FRAME BASICSPASSED\0")]
-    [InlineData("vbl_nmi_timing/2.vbl_timing.nes", 0xE54F, 0xE208,  "VBL TIMINGPASSED")]
+    [InlineData("vbl_nmi_timing/2.vbl_timing.nes", 0xE54F, 0xE208, "VBL TIMINGPASSED")]
     [InlineData("vbl_nmi_timing/3.even_odd_frames.nes", 0xE59F, 0xE258, "EVEN ODD FRAMESPASSED")]
-    [InlineData("vbl_nmi_timing/4.vbl_clear_timing.nes", 0xE535, 0xE1D7, "VBL CLEAR TIMINGFAILED #4")] // Worse, 7 to 4
-    [InlineData("vbl_nmi_timing/5.nmi_suppression.nes", 0xE54C, 0xE200, "NMI SUPPRESSIONFAILED #2")] // Worse, 6 to 2
-    [InlineData("vbl_nmi_timing/6.nmi_disable.nes", 0xE535, 0xE1DA, "NMI DISABLEFAILED #2")] // Worse, 4 to 2
-    [InlineData("vbl_nmi_timing/7.nmi_timing.nes", 0xE58E, 0xE247, "NMI TIMINGFAILED #5")] // Better, 2 to 5
+    [InlineData("vbl_nmi_timing/4.vbl_clear_timing.nes", 0xE535, 0xE1D7, "VBL CLEAR TIMINGPASSED")]
+    [InlineData("vbl_nmi_timing/5.nmi_suppression.nes", 0xE54C, 0xE200, "NMI SUPPRESSIONPASSED")]
+    [InlineData("vbl_nmi_timing/6.nmi_disable.nes", 0xE535, 0xE1DA, "NMI DISABLEPASSED")]
+    [InlineData("vbl_nmi_timing/7.nmi_timing.nes", 0xE58E, 0xE247, "NMI TIMINGPASSED")]
     public void BeExecutedCorrectly(string romName, int poweroffAddress, int printAddress, string expectedResult)
     {
         var ram = new byte[0x10000];
