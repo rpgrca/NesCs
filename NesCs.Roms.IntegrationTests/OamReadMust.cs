9619c6b23eb289f04df7d50e6a2ac116467ec2b9
Roberto <rpgrca@gmail.com>
2023-09-23 14:32:22 -0300

    use test setup for oam_read

--- NesCs.Roms.IntegrationTests/OamReadMust.cs
+++ NesCs.Roms.IntegrationTests/OamReadMust.cs
@@ -1,9 +1,4 @@
-using NesCs.Logic.Cpu;
-using NesCs.Logic.Ppu;
-using NesCs.Logic.Ram;
-using NesCs.Logic.File;
-using NesCs.Logic.Clocking;
-using NesCs.Logic.Tracing;
+using static NesCs.Roms.IntegrationTests.Utilities;
 
 namespace NesCs.Roms.IntegrationTests;
 
@@ -11,40 +6,13 @@ public class OamReadMust
 {
     [Theory]
     [InlineData("oam_read/oam_read.nes", 0xE755, "----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n----------------\n\noam_read\n\nPassed\n")] // ticks 2809549
-    public void BeExecutedCorrectly(string romName, int poweroffAddress, string expectedResult)
+    public void BeExecutedCorrectly(string romName, int powerOffAddress, string expectedResult)
     {
         var ram = new byte[0x10000];
-        var fsp = new FileSystemProxy.Builder().Loading(new NesFileOptions
-        {
-            LoadHeader = true,
-            LoadTrainer = true,
-            LoadProgramRom = true,
-            LoadCharacterRom = true
-        }).Build();
-
-        var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
-        var clock = new Clock(0);
-        var ramController = new RamController.Builder().WithRamOf(ram).PreventRomRewriting().Build();
-        var ppu = new Ppu2C02.Builder().WithRamController(ramController).WithClock(clock).Build();
-        ramController.RegisterHook(ppu);
-
-        var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
-        var cpu = builder
-            .Running(nesFile.ProgramRom)
-            .WithClock(clock)
-            .WithClockDivisorOf(1)
-            .SupportingInvalidInstructions()
-            .WithRamController(ramController)
-            .WithCallback(poweroffAddress, (cpu, _) => cpu.Stop())
-            .TracingWith(new Vm6502DebuggerDisplay())
-            .Build();
-
-        cpu.PowerOn();
-        cpu.Reset();
-        cpu.Run();
+        var clock = CreateSetup(ram, romName, powerOffAddress);
+        clock.Run();
 
         var result = GetString(ram);
-        Assert.Equal(2, nesFile.ProgramRomSize);
         Assert.Equal(0, ram[0x6000]);
         Assert.Equal(expectedResult, result);
     }
