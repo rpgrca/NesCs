5cade641bd18e590d82d58aef8a60b7382326a1b
Roberto <rpgrca@gmail.com>
2023-09-23 15:14:46 -0300

    use test setup even though tests are slower now

--- NesCs.Roms.IntegrationTests/CpuExecSpaceMust.cs
+++ NesCs.Roms.IntegrationTests/CpuExecSpaceMust.cs
@@ -1,8 +1,4 @@
-using NesCs.Logic.Cpu;
-using NesCs.Logic.Ppu;
-using NesCs.Logic.Ram;
-using NesCs.Logic.File;
-using NesCs.Logic.Clocking;
+using static NesCs.Roms.IntegrationTests.Utilities;
 
 namespace NesCs.Roms.IntegrationTests;
 
@@ -10,37 +6,12 @@ public class CpuExecSpaceMust
 {
     [Theory]
     [InlineData("cpu_exec_space/test_cpu_exec_space_ppuio.nes", 0xE7F5, "\u001b[0;37mTEST:test_cpu_exec_space_ppuio\n\u001b[0;33mThis program verifies that the\nCPU can execute code from any\npossible location that it can\naddress, including I/O space.\n\nIn addition, it will be tested\nthat an RTS instruction does a\ndummy read of the byte that\nimmediately follows the\ninstructions.\n\n\u001b[0;37m\u001b[1;34mJSR+RTS TEST OK\nJMP+RTS TEST OK\nRTS+RTS TEST OK\nJMP+RTI TEST OK\nJMP+BRK TEST OK\n\u001b[0;37m\nPassed\n")] // ticks: 3293317
-    [InlineData("cpu_exec_space/test_cpu_exec_space_apu.nes", 0xE976, "", Skip = "Must implement APU, landed at $0234, fail to obey predetermined path #2")]
-    public void BeExecutedCorrectly(string romName, int poweroffAddress, string expectedResult)
+    [InlineData("cpu_exec_space/test_cpu_exec_space_apu.nes", 0xE976, "", Skip = "Must implement APU \u001b[0;37mTEST: test_cpu_exec_space_apu\n\u001b[0;33mThis program verifies that the\nCPU can execute code from any\npossible location that it can\naddress, including I/O space.\n\nIn this test, it is also\nverified that not only all\nwrite-only APU I/O ports\nreturn the open bus, but\nalso the unallocated I/O\nspace in $4018..$40FF.\n\n\u001b[0;37m\u001b[1;34m0022 \b\b\b\b\b4000 \u001b[0;37mERROR\n\u001b[0;33mMysteriously Landed at $0234\u001b[0;37m\nProgram flow did not follow\nthe planned path, for a number\nof different possible reasons.\n\u001b[0;37m\nFailure To Obey Predetermined Execution Path\n\nFailed #2\n")]
+    public void BeExecutedCorrectly(string romName, int powerOffAddress, string expectedResult)
     {
         var ram = new byte[0x10000];
-        var fsp = new FileSystemProxy.Builder().Loading(new NesFileOptions
-        {
-            LoadHeader = true,
-            LoadTrainer = true,
-            LoadProgramRom = true,
-            LoadCharacterRom = true
-        }).Build();
-
-        var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
-        var clock = new Clock(0);
-        var ramController = new RamController.Builder().WithRamOf(ram).PreventRomRewriting().Build();
-        var ppu = new Ppu2C02.Builder().WithRamController(ramController).WithClock(clock).Build();
-        ramController.RegisterHook(ppu);
-
-        var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
-        var cpu = builder
-            .Running(nesFile.ProgramRom)
-            .WithClock(clock)
-            .WithClockDivisorOf(1)
-            .SupportingInvalidInstructions()
-            .WithRamController(ramController)
-            .WithCallback(poweroffAddress, (cpu, _) => cpu.Stop())
-            .Build();
-
-        cpu.PowerOn();
-        cpu.Reset();
-        cpu.Run();
+        var clock = CreateSetup(ram, romName, powerOffAddress);
+        clock.Run();
 
         var result = GetString(ram);
         Assert.Equal(0, ram[0x6000]);
--- NesCs.Roms.IntegrationTests/PpuOpenBusMust.cs
+++ NesCs.Roms.IntegrationTests/PpuOpenBusMust.cs
@@ -1,47 +1,17 @@
-using NesCs.Logic.Cpu;
-using NesCs.Logic.Ppu;
-using NesCs.Logic.Ram;
-using NesCs.Logic.File;
-using NesCs.Logic.Tracing;
-using NesCs.Logic.Clocking;
+using static NesCs.Roms.IntegrationTests.Utilities;
 
 namespace NesCs.Roms.IntegrationTests;
 
 public class PpuOpenBusMust
 {
+    // TODO: On 1:1 CPU:PPU takes 3 second, now takes 8
     [Theory]
     [InlineData("ppu_open_bus/ppu_open_bus.nes", 0xE755, "\nppu_open_bus\n\nPassed\n")] // ticks 89315269
-    public void BeExecutedCorrectly(string romName, int poweroffAddress, string expectedResult)
+    public void BeExecutedCorrectly(string romName, int powerOffAddress, string expectedResult)
     {
         var ram = new byte[0x10000];
-        var fsp = new FileSystemProxy.Builder().Loading(new NesFileOptions
-        {
-            LoadHeader = true,
-            LoadTrainer = true,
-            LoadProgramRom = true,
-            LoadCharacterRom = true
-        }).Build();
-
-        var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
-        var clock = new Clock(0);
-        var ramController = new RamController.Builder().WithRamOf(ram).PreventRomRewriting().Build();
-        var ppu = new Ppu2C02.Builder().WithRamController(ramController).WithClock(clock).Build();
-        ramController.RegisterHook(ppu);
-
-        var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
-        var cpu = builder
-            .Running(nesFile.ProgramRom)
-            .WithClock(clock)
-            .WithClockDivisorOf(1)
-            .SupportingInvalidInstructions()
-            .WithRamController(ramController)
-            .WithCallback(poweroffAddress, (cpu, _) => cpu.Stop())
-            .TracingWith(new Vm6502DebuggerDisplay())
-            .Build();
-
-        cpu.PowerOn();
-        cpu.Reset();
-        cpu.Run();
+        var clock = CreateSetup(ram, romName, powerOffAddress);
+        clock.Run();
 
         var result = GetString(ram);
         Assert.Equal(0, ram[0x6000]);
