82e7c0e4d7bd43c858aeb92c81174a23e4752894
Roberto <rpgrca@gmail.com>
2023-09-23 11:39:42 -0300

    03-dummy_reads fails at 10 instead of 2 now

--- NesCs.Roms.IntegrationTests/InstrMiscMust.cs
+++ NesCs.Roms.IntegrationTests/InstrMiscMust.cs
@@ -9,11 +9,11 @@ namespace NesCs.Roms.IntegrationTests;
 public class InstrMiscMust
 {
     [Theory]
-    [InlineData("instr_misc/rom_singles/01-abs_x_wrap.nes", 0xE7B5, 1, "\n01-abs_x_wrap\n\nPassed\n")] // ticks: 786229
-    [InlineData("instr_misc/rom_singles/02-branch_wrap.nes", 0xE7B5, 1, "\n02-branch_wrap\n\nPassed\n")] // ticks: 793369
-    [InlineData("instr_misc/rom_singles/03-dummy_reads.nes", 0x1, 12, "", Skip = "\nSTA abs,x\n\n03-dummy_reads\n\nFailed #4\n")]
-    [InlineData("instr_misc/rom_singles/04-dummy_reads_apu.nes", 0x1, 12, "", Skip = "failed #2")]
-    public void ReturnPassed(string romName, int poweroffAddress, int clockDivisor, string expectedResult)
+    [InlineData("instr_misc/rom_singles/01-abs_x_wrap.nes", 0xE7B5, "\n01-abs_x_wrap\n\nPassed\n")] // ticks: 786229
+    [InlineData("instr_misc/rom_singles/02-branch_wrap.nes", 0xE7B5, "\n02-branch_wrap\n\nPassed\n")] // ticks: 793369
+    [InlineData("instr_misc/rom_singles/03-dummy_reads.nes", 0x1, "", Skip ="\nROL abs,x\n\n03-dummy_reads\n\nFailed #10\n")]
+    [InlineData("instr_misc/rom_singles/04-dummy_reads_apu.nes", 0x1, "", Skip = "1D 19 11 3D 39 31 5D 59 51 7D 79 71 9D 99 91 BD B9 B1 DD D9 D1 FD F9 F1 1E 3E 5E 7E DE FE BC BE \nOfficial opcodes failed\n\n04-dummy_reads_apu\n\nFailed #2\n")]
+    public void ReturnPassed(string romName, int poweroffAddress, string expectedResult)
     {
         var clock = new Clock(0);
         var ram = new byte[0x10000];
@@ -27,7 +27,11 @@ public class InstrMiscMust
 
         var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
         var ramController = new RamController.Builder().WithRamOf(ram).Build();
-        var ppu = new Ppu2C02.Builder().WithRamController(ramController).WithClock(clock).Build();
+        var ppu = new Ppu2C02.Builder()
+            .WithRamController(ramController)
+            .WithClock(clock)
+            .WithClockDivisorOf(1)
+            .Build();
         ramController.RegisterHook(ppu);
 
         var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
@@ -36,7 +40,7 @@ public class InstrMiscMust
             .SupportingInvalidInstructions()
             .WithRamController(ramController)
             .WithClock(clock)
-            .WithClockDivisorOf(clockDivisor)
+            .WithClockDivisorOf(3)
             .WithCallback(poweroffAddress, (cpu, _) => cpu.Stop())
             .Build();
 
