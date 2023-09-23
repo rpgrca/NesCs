a9a994fab4752877c8b993fb9668bb15ee1a93bd
Roberto <rpgrca@gmail.com>
2023-09-23 11:25:36 -0300

    clean up nestest, ppu fails for 1 position X

--- NesCs.SystemTests/Nestest/Program.cs
+++ NesCs.SystemTests/Nestest/Program.cs
@@ -3,6 +3,7 @@ using NesCs.Logic.Clocking;
 using NesCs.Logic.Ppu;
 using NesCs.Logic.Ram;
 using NesCs.Logic.Tracing;
+using NesCs.Logic.Nmi;
 
 if (args.Length < 1)
 {
@@ -12,7 +13,8 @@ if (args.Length < 1)
 
 Console.WriteLine($"Loading {args[0]}...");
 
-var fsp = new FileSystemProxy.Builder().Loading(new NesFileOptions
+var fsp = new FileSystemProxy.Builder()
+    .Loading(new NesFileOptions
     {
         LoadHeader = true,
         LoadTrainer = true,
@@ -25,82 +27,56 @@ var nesFile = fsp.Load(args[0]);
 Console.WriteLine($"Loaded!");
 Console.WriteLine(nesFile.ToString());
 
-var memory = new byte[0x10000];
-var ramController = new RamController.Builder()
-    .WithRamOf(memory)
-    .Build();
-
 var clock = new Clock(0);
+var rasterAddress = new RasterAddress();
+var nmiGenerator = new NmiGenerator(clock, rasterAddress);
+var ramController = new RamController.Builder().Build();
 var ppu = new Ppu2C02.Builder()
     .WithRamController(ramController)
     .WithClock(clock)
+    .WithNmiGenerator(nmiGenerator)
+    .WithRaster(rasterAddress)
+    .WithClockDivisorOf(1)
     .Build();
+ramController.RegisterHook(ppu);
 
-var builder = new Cpu6502.Builder()
-    .ProgramMappedAt(0x8000); // NROM-256 or NROM-128
-
+var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
 if (nesFile.ProgramRomSize == 1)
 {
     builder.ProgramMappedAt(0xC000); // NROM-128
 }
 
-        const int PrintAddress = 0xE463; // 0xE558;
-        const int PowerOffAddress = 0xE7B5; // 0xE7E9;
-        var message = string.Empty;
-
 var cpu = builder
     .Running(nesFile.ProgramRom)
+    .WithClock(clock)
     .SupportingInvalidInstructions()
+    .WithRamController(ramController)
     .WithProgramCounterAs(0xC000) // 0xC000 -> nesttest batch, 0xC004 normal game
     //.WithProcessorStatusAs(ProcessorStatus.X | ProcessorStatus.I) -> Not for nestest
-    .WithClock(clock)
-    .WithRamController(ramController)
     .TracingWith(new Vm6502DebuggerDisplay())
-    .WithCallback(0xC66E, (cpu, _) => {
-        var h2 = cpu.PeekMemory(0x02);
-        var h3 = cpu.PeekMemory(0x03);
-
-        var (P, A, PC, X, Y, S, CY) = cpu.TakeSnapshot();
-
-        if (A == 0 && X == 0xFF && Y == 0x15 && S == 0xFD && (byte)P == 0x27 && h2 == 0 && h3 == 0)
-        {
-            Console.WriteLine("\nReached end successfully");
-        }
-        else
-        {
-            Console.WriteLine($"\nFailed! $02: {h2}, $03: {h3}");
-        }
-        cpu.Stop();
-    })
-    .WithCallback(PrintAddress, (cpu, _) => {
-        var c = (char)cpu.ReadByteFromAccumulator();
-        Console.Write($"{c}");
-        System.Diagnostics.Debug.Print($"{c}");
-        message += c;
-    })
+    .WithClockDivisorOf(3)
+    .WithCallback(0xC66E, (cpu, _) =>
+    cpu.Stop()
+    )
     .Build();
 
-    var lowByte = 0;
-    var highByte = 0;
-    var firstWrite = true;
-
-ramController.AddHook(0x2006, (a, v) => {
-    if (firstWrite)
-    {
-        lowByte = v;
-        firstWrite = false;
-    }
-    else
-    {
-        highByte = v;
-        firstWrite = true;
-    }
-
-    message += (char)v;
-});
-
+nmiGenerator.AttachTo(cpu);
 cpu.PowerOn();
 cpu.Reset();
 clock.Run();
+
+var (P, A, PC, X, Y, S, CY) = cpu.TakeSnapshot();
+var h2 = cpu.PeekMemory(0x02);
+var h3 = cpu.PeekMemory(0x03);
+
+if (A == 0 && X == 0xFF && Y == 0x15 && S == 0xFD && (byte)P == 0x27 && h2 == 0 && h3 == 0 && CY == 26554)
+{
+    Console.WriteLine("\nCPU executed correctly");
+}
+
+if (rasterAddress.Y == 233 && rasterAddress.X == 209)
+{
+    Console.WriteLine("\nPPU executed correctly");
+}
 
 return 0;
