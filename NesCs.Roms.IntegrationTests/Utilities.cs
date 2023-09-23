f6e44118c156dccf9de5e8996af330a0e428b081
Roberto <rpgrca@gmail.com>
2023-09-23 13:22:50 -0300

    refactor static functions for builder

--- NesCs.Roms.IntegrationTests/Utilities.cs
+++ NesCs.Roms.IntegrationTests/Utilities.cs
@@ -8,9 +8,11 @@ namespace NesCs.Roms.IntegrationTests;
 
 public static class Utilities
 {
-    public static IClock CreateSetup(byte[] ram, string romName, int powerOffAddress)
+    private const int CpuDivisor = 3;
+    private const int PpuDivisor = 1;
+
+    private static INesFile LoadNesFile(string romName)
     {
-        var clock = new Clock(0);
         var fsp = new FileSystemProxy.Builder().Loading(new NesFileOptions
         {
             LoadHeader = true,
@@ -19,31 +21,76 @@ public static class Utilities
             LoadCharacterRom = true
         }).Build();
 
-        var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
-        var ramController = new RamController.Builder()
+        return fsp.Load("../../../../../nes-test-roms/" + romName);
+    }
+
+    private static IRamController CreateRamController(byte[] ram) =>
+        new RamController.Builder()
             .WithRamOf(ram)
             .Build();
 
+    private static IPpu CreatePpu(IClock clock, IRamController ramController)
+    {
         var ppu = new Ppu2C02.Builder()
             .WithRamController(ramController)
             .WithClock(clock)
-            .WithClockDivisorOf(1)
+            .WithClockDivisorOf(PpuDivisor)
             .Build();
 
         ramController.RegisterHook(ppu);
+        return ppu;
+    }
+
+    private static Cpu6502.Builder CreateCpu(IClock clock, INesFile nesFile, IRamController ramController)
+    {
         var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
-        var cpu = builder
+        if (nesFile.ProgramRomSize == 1)
+        {
+            builder.ProgramMappedAt(0xC000); // NROM-128
+        }
+
+        return builder
             .Running(nesFile.ProgramRom)
             .SupportingInvalidInstructions()
             .WithRamController(ramController)
             .WithClock(clock)
-            .WithClockDivisorOf(3)
+            .WithClockDivisorOf(CpuDivisor);
+    }
+
+    private static Cpu6502 CreateCpuWith(IClock clock, INesFile nesFile, IRamController ramController, int powerOffAddress) =>
+        CreateCpu(clock, nesFile, ramController)
             .WithCallback(powerOffAddress, (cpu, _) => cpu.Stop())
             .Build();
 
+    private static Cpu6502 CreateCpuWith(IClock clock, INesFile nesFile, IRamController ramController, int poweroffAddress, int resetAddress) =>
+        CreateCpu(clock, nesFile, ramController)
+            .WithCallback(poweroffAddress, (cpu, _) => cpu.Stop())
+            .WithCallback(resetAddress, (cpu, _) => cpu.Reset())
+            .Build();
+
+    public static IClock CreateSetup(byte[] ram, string romName, int powerOffAddress)
+    {
+        var clock = new Clock(0);
+        var nesFile = LoadNesFile(romName);
+        var ramController = CreateRamController(ram);
+        var ppu = CreatePpu(clock, ramController);
+        var cpu = CreateCpuWith(clock, nesFile, ramController, powerOffAddress);
+
         cpu.PowerOn();
         cpu.Reset();
+        return clock;
+    }
 
+    public static IClock CreateSetup(byte[] ram, string romName, int powerOffAddress, int resetAddress)
+    {
+        var clock = new Clock(0);
+        var nesFile = LoadNesFile(romName);
+        var ramController = CreateRamController(ram);
+        var ppu = CreatePpu(clock, ramController);
+        var cpu = CreateCpuWith(clock, nesFile, ramController, powerOffAddress, resetAddress);
+
+        cpu.PowerOn();
+        cpu.Reset();
         return clock;
     }
 }
