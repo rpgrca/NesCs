e1ada23664a693633ad28a87c4c8658c222bdac6
Roberto <rpgrca@gmail.com>
2023-09-23 13:23:32 -0300

    use new setup builder

--- NesCs.Roms.IntegrationTests/CpuResetMust.cs
+++ NesCs.Roms.IntegrationTests/CpuResetMust.cs
@@ -1,8 +1,4 @@
-using NesCs.Logic.Cpu;
-using NesCs.Logic.Ppu;
-using NesCs.Logic.Ram;
-using NesCs.Logic.File;
-using NesCs.Logic.Clocking;
+using static NesCs.Roms.IntegrationTests.Utilities;
 
 namespace NesCs.Roms.IntegrationTests;
 
@@ -13,34 +9,9 @@ public class CpuResetMust
     [InlineData("cpu_reset/registers.nes", 0xE29C, 0xE9D5, "A  X  Y  P  S\n34 56 78 FF 0F \n\nregisters\n\nPassed\n")] // ticks: 57487249
     public void ReturnPassed(string romName, int resetAddress, int poweroffAddress, string expectedResult)
     {
-        var clock = new Clock(0);
         var ram = new byte[0x10000];
-        var fsp = new FileSystemProxy.Builder().Loading(new NesFileOptions
-        {
-            LoadHeader = true,
-            LoadTrainer = true,
-            LoadProgramRom = true,
-            LoadCharacterRom = true
-        }).Build();
-
-        var nesFile = fsp.Load("../../../../../nes-test-roms/" + romName);
-        var ramController = new RamController.Builder().WithRamOf(ram).Build();
-        var ppu = new Ppu2C02.Builder().WithRamController(ramController).WithClock(clock).Build();
-
-        var builder = new Cpu6502.Builder().ProgramMappedAt(0x8000);
-        var cpu = builder
-            .Running(nesFile.ProgramRom)
-            .SupportingInvalidInstructions()
-            .WithRamController(ramController)
-            .WithClock(clock)
-            .WithClockDivisorOf(1)
-            .WithCallback(resetAddress, (cpu, _) => cpu.Reset())
-            .WithCallback(poweroffAddress, (cpu, _) => cpu.Stop())
-            .Build();
-
-        cpu.PowerOn();
-        cpu.Reset();
-        cpu.Run();
+        var clock = CreateSetup(ram, romName, poweroffAddress, resetAddress);
+        clock.Run();
 
         var result = GetString(ram);
         Assert.Equal(0, ram[0x6000]);
